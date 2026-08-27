d766ef6575cb5ad648d32ab6dea2e5a292e782e8
Parthsarthi Joshi <parthucmas@gmail.com>
2026-08-27 17:40:46 +0530

    mlflow setup

--- src/predict.py
+++ src/predict.py
@@ -1,15 +1,15 @@
-import joblib
+import mlflow
+import mlflow.sklearn
 import pandas as pd
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+
 # Load saved model
-model = joblib.load(
-"models/linear_reg_model.pkl"
+model = mlflow.sklearn.load_model(
+    "models:/Sales_Prediction_Model@champion"
 )
 # New observation
-new_data = pd.DataFrame({
-"TV": [35],
-"Radio": [50000],
-"Newspaper": [8]
-})
+new_data = pd.DataFrame({"TV": [35],"Radio": [50000],"Newspaper": [8]})
 # Prediction
 prediction = model.predict(new_data)
 print("Prediction:", prediction[0])
