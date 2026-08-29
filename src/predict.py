7e8c795a0f2b794264c3adc463b52257f6cf5bdd
Parthsarthi Joshi <parthucmas@gmail.com>
2026-08-29 14:31:44 +0530

    removed linting

--- src/predict.py
+++ src/predict.py
@@ -9,7 +9,7 @@ model = mlflow.sklearn.load_model(
     "models:/Sales_Prediction_Model@champion"
 )
 # New observation
-new_data = pd.DataFrame({"TV": [35],"Radio": [50000],"Newspaper": [8]})
+new_data = pd.DataFrame({"TV": [35], "Radio": [50000], "Newspaper": [8]})
 # Prediction
 prediction = model.predict(new_data)
 print("Prediction:", prediction[0])
